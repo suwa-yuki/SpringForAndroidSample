0b986f92db73b068ab36a1b9c0fe69f7b9945d36
suwa <suwa>
2013-03-15 14:55:41 +0900

    delete files

--- gen/jp/classmethod/android/sample/springforandroid/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package jp.classmethod.android.sample.springforandroid;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
